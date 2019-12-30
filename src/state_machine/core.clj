40fc05cd8d5eef0083341dfec107960d237a91a4
rdgd <ryan.is.gray@gmail.com>
2019-12-30 15:01:26 -0600

    Handling nested state machines.

--- src/state_machine/core.clj
+++ src/state_machine/core.clj
@@ -1,7 +1,7 @@
 (ns state-machine.core)
 
 (defn get-state-by-name
-  [state-defs state-name]
+  [state-defs state-name & [context]]
   (some (fn [sd] (when (= state-name (:name sd)) sd)) state-defs))
 
 (defn mk-history-entry
@@ -20,15 +20,30 @@
                            (conj history (mk-history-entry current-state acc status)))
      :current-value      acc}))
 
+(defn init
+  [state-defs]
+  {:states state-defs
+   :history []})
+
+(defn merge-state-wrappers
+  [wrapper-target wrapper]
+  (assoc wrapper-target
+    :history (concat (:history wrapper-target) (:history wrapper))
+    :current-value (get wrapper :current-value)))
+
 (defn invoke-state
-  [state-wrapper acc & [state-name]]
-  (println state-name)
+  [state-wrapper acc & [state-name context]]
   (let [states (:states state-wrapper)]
-    (if-let [{:keys [name handler stop-after on-fail unless automatic on-success] :as state} (get-state-by-name states state-name)]
+    (if-let [{:keys [name handler stop-after on-fail unless automatic on-success] :as state} (get-state-by-name states state-name context)]
       (if (and unless (unless acc))
         (update-wrapper state-wrapper acc name true)
-        (let [res (handler acc)
-              updated-state-wrapper (update-wrapper state-wrapper res state (if res :success :fail))]
+        (let [nested? (sequential? handler)
+              res (if nested?
+                    (invoke-state (init handler) acc (:name (first handler)) name)
+                    (handler state-wrapper state acc))
+              updated-state-wrapper (if nested?
+                                      (update-wrapper (merge-state-wrappers state-wrapper res) (:current-value res) state (if res :success :fail))
+                                      (update-wrapper state-wrapper res state (if res :success :fail)))]
           (cond
             (and (not res) on-fail) (invoke-state updated-state-wrapper acc on-fail)
             (and (not res) (not on-fail)) (throw (Error. (str "Failure occurred but no handler for state " state ". Data at time of failure " acc)))
@@ -37,10 +52,7 @@
             :else (partial invoke-state updated-state-wrapper res on-success))))
       (throw (AssertionError. (str "State \"" state-name "\" does not exist"))))))
 
-(defn init
-  [state-defs]
-  {:states state-defs
-   :history []})
+
 
 (defn start
   [state name acc]
