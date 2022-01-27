197cfdcb295be8e4d93033e4c1cd7e9931a1252a
rdgd <ryan.is.gray@gmail.com>
2022-01-27 09:36:17 -0600

    update

--- src/state_machine/core.clj
+++ src/state_machine/core.clj
@@ -1,47 +1,53 @@
 (ns state-machine.core)
 
 (defn get-state-by-name
-  "Search state definitions for state by name"
   [schema state-name]
   (some
-    (fn [{:keys [handler] :as sd}]
+    (fn [{:keys [output] :as sd}]
       (cond
         (= state-name (:name sd)) sd
-        (sequential? handler) (get-state-by-name handler state-name)))
+        (sequential? output) (get-state-by-name output state-name)))
     schema))
 
 (defn get-state-parent
   [schema state-name & [parent]]
   (reduce
-    (fn [acc {:keys [name handler]}]
+    (fn [acc {:keys [name output]}]
       (cond
         (= name state-name) parent
-        (sequential? handler) (get-state-parent handler state-name name)
+        (sequential? output) (get-state-parent output state-name name)
         :else acc)) nil schema))
 
-(defn invoke-state
+(defn next-state
+  [sm transition output-val]
+  (if (keyword? transition)
+    transition
+    (transition sm output-val)))
+
+(defn activate
   [{:keys [schema state-name] :as sm}]
-  (if-let [{:keys [name handler validator stop-after on-fail stop-before automatic? on-success]}
+  (if-let [{:keys [name output valid? halt-after? halt-before? automatic? transition]}
            (get-state-by-name schema state-name)]
     (cond
-      (and stop-before (stop-before sm)) (assoc sm :state-name name)
-      (sequential? handler) (invoke-state (assoc sm :state-name (-> handler first :name)))
-      :else (let [res (handler sm)
-                  success-args (assoc sm :state-name on-success :acc (:acc res))]
+      (and halt-before? (halt-before? sm)) (assoc sm :state-name name)
+      (sequential? output) (activate (assoc sm :state-name (-> output first :name)))
+      :else (let [output-val (output sm)
+                  success-args (-> sm
+                                   (assoc :state-name (next-state sm transition output-val) :acc (:acc output-val))
+                                   #_(update :history (fn [history] (conj (or history []) (dissoc sm :schema)))))]
               (cond
-                (and (not res) on-fail) (invoke-state (assoc sm :state-name on-fail))
-                (and (not res) (not on-fail)) (throw (Error. (str "Missing on-fail handler for state " state-name)))
-                (and validator (not (validator res))) sm
-                (and stop-after (stop-after res)) (if-let [sn (get-state-parent schema state-name)]
-                                                    (assoc success-args :state-name (:on-success (get-state-by-name schema sn)))
-                                                    success-args)
-                automatic? (invoke-state success-args)
+                (and valid? (not (valid? output-val))) sm
+                (and halt-after? (halt-after? output-val))
+                (if-let [sn (get-state-parent schema state-name)]
+                  (assoc success-args :state-name (next-state sm (:transition (get-state-by-name schema sn)) output-val))
+                  success-args)
+                automatic? (activate success-args)
                 :else success-args)))
     (throw (AssertionError. (str "State \"" state-name "\" does not exist")))))
 
 (defn start
   "Convenience function to invoke first state in schema"
   [schema acc]
-  (invoke-state {:schema schema
-                 :acc acc
-                 :state-name (-> schema first :name)}))
+  (activate {:schema schema
+             :acc acc
+             :state-name (-> schema first :name)}))
--- src/state_machine/core_test.clj
+++ src/state_machine/core_test.clj
@@ -3,62 +3,50 @@
             [bond.james :as bond :refer [with-spy]]
             [clojure.test :refer :all]))
 
-;; Idea: Interceptors for before and after each handler? Can be sequential to apply multiple middlewares
+;; Idea: Interceptors for before and after each output fn? Can be sequential to apply multiple middlewares
 ;; high-level/general middleware pattern too?
-
 (defn make-automatic
   [schema]
   (mapv (fn [x]
-          (if (sequential? (:handler x))
-            (assoc x :handler (make-automatic (:handler x)))
+          (if (sequential? (:output x))
+            (assoc x :output (make-automatic (:output x)))
             (assoc x :automatic? true))) schema))
 
-(defn stop?
-  [sm]
-  (> (:acc sm) 100))
-
-(defn inc-acc [sm]
-  (update sm :acc inc))
-
+(defn halt?  [sm] (> (:acc sm) 100))
+(defn inc-acc [sm] (update sm :acc inc))
 (def start inc-acc)
 (def first-nested inc-acc)
 (def last-nested inc-acc)
-
 (defn second-nested
-  [{:keys [handler-arg acc] :as sm}]
-  (assoc sm :acc (* (or handler-arg 4) acc)))
+  "The magic 4 is so that the automatic test run will reach a conclusion more quickly by satisfying the halt? condition. We have to have something there regardless, or we will npe."
+  [{:keys [input acc] :as sm}]
+  (assoc sm :acc (* (or input 4) acc)))
 
 (def schema [{:name          :start
-              :handler       #'start
-              :on-success    :start-nested}
+              :output        #'start
+              :transition    :start-nested}
              {:name       :start-nested
-              :on-success :end
-              :handler [{:name :first-nested
-                         :handler #'first-nested
-                         :on-success :second-nested}
+              :transition :end
+              :output [{:name :first-nested
+                         :output #'first-nested
+                         :transition :second-nested}
                         {:name :second-nested
-                         :handler #'second-nested
-                         :on-success :last-nested}
+                         :output #'second-nested
+                         :transition :last-nested}
                         {:name :last-nested
-                         :handler #'last-nested
-                         :stop-after #'stop?
-                         :on-success :first-nested}]}
+                         :output #'last-nested
+                         :halt-after? #'halt?
+                         :transition :first-nested}]}
              {:name       :end
-              :handler    (fn [x] (println "This is the end") x)
-              :stop-after (fn [x] x)
+              :output    #(do (println "This is the end") %)
+              :halt-after? (constantly true)
               :automatic  false}])
-
-(deftest get-state-parent-test
-  (testing "Finds top level states with no parent"
-    (is (nil? (c/get-state-parent schema :start-nested))))
-  (testing "Finds nested state with parent"
-    (is (= :start-nested (c/get-state-parent schema :last-nested)))))
 
 (deftest start-test
   (testing "start function"
     (with-spy [start]
       (c/start schema 0)
-      (is (= 1 (-> start bond/calls count)) "automatically calls the first state handler"))))
+      (is (= 1 (-> start bond/calls count)) "automatically calls the first state output fn"))))
 
 (deftest manual-transition-test
   (testing "Automatic transition does not occur"
@@ -67,7 +55,7 @@
       (is (= 0 (-> first-nested bond/calls count)))))
   (testing "Manual transition works"
     (with-spy [first-nested]
-      (let [s (c/invoke-state (c/start schema 0))]
+      (let [s (c/activate (c/start schema 0))]
         (is (= 1 (-> first-nested bond/calls count)))
         (is (= (:state-name s) :second-nested))))))
 
@@ -79,16 +67,17 @@
 (deftest nested-state-machine-test
   (testing "Nested state machines are reflected at the top-level wrapper"
     (let [started (c/start schema 0)
-          s (c/invoke-state started)
-          ss (c/invoke-state (assoc s :handler-arg 2))]
+          s (c/activate started)
+          ss (c/activate (assoc s :input 2))]
       (is (= (:state-name ss) :last-nested)))))
 
-(deftest passing-handler-args-test
-  (testing "Passing handler args"
-    (let [started (c/start schema 0)
-          s (c/invoke-state started)
-          ss (c/invoke-state (assoc s :handler-arg 2))]
-      (is (= (:acc ss) 4)))))
+(deftest passing-inputs-test
+  (testing "Passing inputs"
+    (is (= (-> (c/start schema 0)
+               (c/activate)
+               (assoc :input 2)
+               (c/activate)
+               :acc) 4))))
 
 (deftest get-state-by-name-test
   (testing "Getting state by name works"
@@ -96,4 +85,10 @@
   (testing "Getting nested states works"
     (is (= {:name :foo}
            (c/get-state-by-name [{:name :bar}
-                                 {:name :baz :handler [{:name :foo}]}] :foo)))))
+                                 {:name :baz :output [{:name :foo}]}] :foo)))))
+
+(deftest get-state-parent-test
+  (testing "Finds top level states with no parent"
+    (is (nil? (c/get-state-parent schema :start-nested))))
+  (testing "Finds nested state with parent"
+    (is (= :start-nested (c/get-state-parent schema :last-nested)))))
